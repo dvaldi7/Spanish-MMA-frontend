84b968c6f2e1e1c0ebd5fc52c1230d825eae00b7
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-10-21 18:38:33 +0200

    added login for admin user

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
+import Login from './components/Login'
 
 function App() {
 
   return (
-
-    <h1 className="text-3xl font-bold underline text-red-600 text-center">
-      Hola Tailwind!
-    </h1>
-
+    <>
+      <Login />
+    </>
   )
 }
 
