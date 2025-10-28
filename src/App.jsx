790c7375d8cc2937272f350bb9c1fd8738987112
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-10-28 10:57:48 +0100

    fixed homepage and 404 text

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Layout from './components/Layout.jsx';
 
 const HomePage = () => (
   <div className="text-center mt-20">
-    <h1 className="text-4xl font-extrabold text-gray-800 mb-4">Bienvenido al Catálogo de MMA EN Español</h1>
-    <p className="text-xl text-gray-600">últimas noticias</p>
+    <h1 className="text-4xl font-extrabold bg-gradient-to-b from-custom-red to-custom-gold bg-clip-text text-transparent rounded-md">Bienvenido al Catálogo de MMA EN Español</h1>
+    <p className="text-xl bg-gradient-to-b from-custom-red to-custom-gold bg-clip-text text-transparent rounded-md">últimas noticias</p>
   </div>
 );
 
@@ -30,8 +30,10 @@ function App() {
                 </ProtectedRoute>
               } />
 
-              <Route path="*" element={<div className='bg-gray-200 rounded-xl w-auto h-auto mt-14 sm:w-3/4 sm:ml-24'>
-                                          <h1 className="text-center mt-5 text-3xl flex justify-center items-center">
+              <Route path="*" element={<div className='bg-custom-black rounded-xl w-auto h-auto mt-14 sm:w-3/4 sm:ml-24'>
+                                          <h1 className="text-center mt-5 text-3xl flex justify-center 
+                                          items-center bg-gradient-to-b from-custom-red to-custom-gold 
+                                          bg-clip-text text-transparent font-extrabold">
                                             404 - Página no encontrada
                                           </h1>
                                       </div>} />
