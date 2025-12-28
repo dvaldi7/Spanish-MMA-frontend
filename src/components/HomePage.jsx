e0e964b0a466cf1d29779b7106a25d8a7307ca75
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-12-28 18:20:49 +0100

    fixed 404 error page

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const HomePage = () => {
+
+    return (
+        <main className="text-center mt-16 p-4 max-w-4xl mx-auto">
+            {/* Título Principal */}
+            <h1 className="text-5xl md:text-6xl font-extrabold mb-6 leading-tight">
+                <span className="bg-gradient-to-r from-red-600 to-amber-500 bg-clip-text text-transparent ">
+                    El Catálogo definitivo de MMA en Español 
+                </span>
+            </h1>
+
+            {/* Subtítulo */}
+            <h2 className="text-xl md:text-2xl bg-gradient-to-r from-red-600 to-amber-500 bg-clip-text 
+        text-transparent font-medium mb-8">
+                Explora la comunidad española de las Artes Marciales Mixtas de España
+            </h2>
+
+            {/* NOTICIAS */}
+
+        </main>
+
+    )
+}
