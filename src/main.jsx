dc66d1a5f344cd21d21a50cea1a6d9d28723d691
Daniel Valdivieso-M <dvaldi7@gmail.com>
2026-02-26 10:56:23 +0100

    added HelmetProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ReactGA from "react-ga4"
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactGA.initialize("G-M68KDZ6WQK");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
