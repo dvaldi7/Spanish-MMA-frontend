c9dbf470114d03b1dfa9faccd44c456b61745194
Daniel Valdivieso <dvaldi7@gmail.com>
2026-09-17 14:02:47 +0000

    Use VITE_BACKEND_URL for API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.PROD
-    ? 'https://spanish-mma-backend.onrender.com/api'
-    : 'http://localhost:3001/api';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001';
+const baseURL = `${BACKEND_URL.replace(/\/$/, '')}/api`;
 
 const api = axios.create({
     baseURL: baseURL,
